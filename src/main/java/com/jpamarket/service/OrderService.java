9f6e6711c168bc873ebac44768d3fcc72945299c
sy_patrick <booth20037@gmail.com>
2024-02-24 03:55:59 +0900

    Api Controller 추가 및 Service 수정

--- src/main/java/com/jpamarket/service/OrderService.java
+++ src/main/java/com/jpamarket/service/OrderService.java
@@ -46,8 +46,7 @@ public class OrderService {
     /**
      * 취소
      */
-    @Transactional
-    public void cancelOrder(Long orderId){
+    @Transactional    public void cancelOrder(Long orderId){
         Order order = orderRepository.findOne(orderId);
 
         order.cancel();
